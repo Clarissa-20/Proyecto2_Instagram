e00f1e17716da56f16f8e564e8b307ea1c12c394
Clarissa Ramos <angie.ramos20137@gmail.com>
2026-03-01 17:52:26 -0600

    clase marcomobile creada

---
+++ Proyecto2_Instagram/src/InstaGui/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package InstaGui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author HP
+ */
+public class Login extends JPanel{
+    
+    
+    public Login(MarcoMobile parent){
+        
+    }
+}
